e5dedb732b269abf3a23ecce45fe7ba24f2d557c
Steve Ernstberger <steve32285@gmail.com>
2023-04-19 20:58:30 -0400

    try again

---
+++ apps/bnb/public/data/listings.ts
@@ -0,0 +1,164 @@
+const listings = [
+  {
+    id: 1,
+    title: 'Modern apartment in the city center',
+    price: 120,
+    image: '/images/1.jpg',
+    description:
+      'This beautiful apartment is located in the heart of the city and features modern amenities and breathtaking views.',
+  },
+  {
+    id: 2,
+    title: 'Luxury villa with private pool',
+    price: 350,
+    image: '/images/2.jpg',
+    description:
+      'Escape to this stunning villa and enjoy your own private oasis with a pool, spa, and lush gardens.',
+  },
+  {
+    id: 3,
+    title: 'Cozy cabin in the woods',
+    price: 80,
+    image: '/img/listing3.jpg',
+    description:
+      'Get away from it all and enjoy the peace and quiet of nature in this charming cabin.',
+  },
+  {
+    id: 4,
+    title: 'Beachfront bungalow with ocean views',
+    price: 200,
+    image: '/img/listing4.jpg',
+    description:
+      'Relax and unwind in this beautiful bungalow with panoramic ocean views.',
+  },
+  {
+    id: 5,
+    title: 'Spacious loft in trendy neighborhood',
+    price: 150,
+    image: '/img/listing5.jpg',
+    description:
+      'Experience the best of city living in this stylish loft with plenty of space and natural light.',
+  },
+  {
+    id: 6,
+    title: 'Rustic farmhouse with mountain views',
+    price: 100,
+    image: '/img/listing6.jpg',
+    description:
+      'Escape to the countryside and enjoy the tranquility of this charming farmhouse.',
+  },
+  {
+    id: 7,
+    title: 'Chic studio in the heart of downtown',
+    price: 90,
+    image: '/img/listing7.jpg',
+    description:
+      'Live like a local in this modern studio in the heart of the city.',
+  },
+  {
+    id: 8,
+    title: 'Historic townhouse with garden',
+    price: 180,
+    image: '/img/listing8.jpg',
+    description:
+      'Step back in time and enjoy the elegance and charm of this historic townhouse.',
+  },
+  {
+    id: 9,
+    title: 'Sleek apartment with rooftop terrace',
+    price: 250,
+    image: '/img/listing9.jpg',
+    description:
+      'Take in stunning views of the city from this modern apartment with a rooftop terrace.',
+  },
+  {
+    id: 10,
+    title: 'Cottage in the countryside',
+    price: 70,
+    image: '/img/listing10.jpg',
+    description:
+      'Experience the peace and quiet of rural living in this cozy cottage.',
+  },
+  {
+    id: 11,
+    title: 'Penthouse with panoramic views',
+    price: 500,
+    image: '/img/listing11.jpg',
+    description:
+      'Live like royalty in this luxurious penthouse with stunning panoramic views of the city.',
+  },
+  {
+    id: 12,
+    title: 'Secluded beach house with private access',
+    price: 300,
+    image: '/img/listing12.jpg',
+    description:
+      'Escape to this secluded beach house and enjoy your own private access to the sand and surf.',
+  },
+  {
+    id: 13,
+    title: 'Stylish apartment with city views',
+    price: 200,
+    image: '/img/listing13.jpg',
+    description:
+      'Enjoy the best of urban living in this chic apartment withstunning city views.',
+  },
+  {
+    id: 14,
+    title: 'Quaint cottage with garden',
+    price: 90,
+    image: '/img/listing14.jpg',
+    description:
+      'Relax in the charming garden of this cozy cottage and enjoy the simple life.',
+  },
+  {
+    id: 15,
+    title: 'Elegant townhouse in historic district',
+    price: 280,
+    image: '/img/listing15.jpg',
+    description:
+      'Step back in time and enjoy the grandeur of this elegant townhouse in a historic district.',
+  },
+  {
+    id: 16,
+    title: 'Sleek apartment with city skyline views',
+    price: 220,
+    image: '/img/listing16.jpg',
+    description:
+      'Experience the excitement of city living in this modern apartment with stunning skyline views.',
+  },
+  {
+    id: 17,
+    title: 'Charming cottage in the countryside',
+    price: 120,
+    image: '/img/listing17.jpg',
+    description:
+      'Experience the beauty and tranquility of the countryside in this charming cottage.',
+  },
+  {
+    id: 18,
+    title: 'Luxury apartment with river views',
+    price: 400,
+    image: '/img/listing18.jpg',
+    description:
+      'Indulge in luxury and enjoy breathtaking river views from this stunning apartment.',
+  },
+  {
+    id: 19,
+    title: 'Rustic cabin in the woods',
+    price: 60,
+    image: '/img/listing19.jpg',
+    description:
+      'Escape from the hustle and bustle of city life and enjoy the peace and quiet of this rustic cabin.',
+  },
+  {
+    id: 20,
+    title: 'Stylish loft with industrial charm',
+    price: 180,
+    image: '/img/listing20.jpg',
+    description:
+      'Experience the best of urban living in this stylish loft with industrial charm and modern amenities.',
+  },
+];
+
+export default listings;
