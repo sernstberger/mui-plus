743e19e5d0777d66ee976dc52f421fad0fb7782e
Steve Ernstberger <steve32285@gmail.com>
2023-04-30 09:39:05 -0400

    added first few fields

--- apps/crud/src/app/app.tsx
+++ apps/crud/src/app/app.tsx
@@ -1,15 +1,24 @@
-import { Form } from '@mui-plus/form'
+import { Form, Input, Select } from '@mui-plus/form';
+import { Container } from '@mui/material';
 
 export function App() {
-  return <div>
-
-
-
-
-    <Form onSubmit={() => { }}>
-      <div>hello</div>
-    </Form>
-  </div>;
+  return (
+    <Container maxWidth="sm">
+      <Form onSubmit={() => { }}>
+        <Input fieldName="firstName" label="First name" />
+        <Input fieldName="lastName" label="Last name" />
+        <Select
+          fieldName="iceCream"
+          label="Ice Cream"
+          options={[
+            { label: 'Chocolate', value: 'chocolate' },
+            { label: 'Vanilla', value: 'vanilla' },
+            { label: 'Strawberry', value: 'strawberry' },
+          ]}
+        />
+      </Form>
+    </Container>
+  );
 }
 
 export default App;
