f0bc55902ed6b4246872e14ac9d1d751fdd7f8b0
Steve Ernstberger <steve32285@gmail.com>
2023-04-30 11:38:55 -0400

    created step component

--- apps/crud/src/app/app.tsx
+++ apps/crud/src/app/app.tsx
@@ -1,70 +1,42 @@
-import {
-  Address,
-  Checkbox,
-  Email,
-  Form,
-  Input,
-  Select,
-} from '@mui-plus/form';
-import { Container } from '@mui/material';
-import { Route, Routes } from 'react-router-dom';
+import { Address, Checkbox, Email, Form, Input, Select } from '@mui-plus/form';
+import { Container, Typography } from '@mui/material';
+import { Link, Outlet, Route, Routes } from 'react-router-dom';
 import type { RootState } from '../app/store';
 import { useSelector, useDispatch } from 'react-redux';
 import { decrement, increment } from './components/Step/stepSlice';
 import ProgressBar from './components/ProgressBar';
+import Step from './components/Step';
 
 export function App() {
   const count = useSelector((state: RootState) => state.step.value);
   const dispatch = useDispatch();
 
-
-
   return (
     <Routes>
       <Route index element={<h1>Home</h1>} />
       <Route
-        path="/form"
+        path="form"
         element={
           <Container maxWidth="sm">
             <ProgressBar />
             <br />
 
-            <Form onSubmit={() => { }}>
-              <Input fieldName="firstName" label="First name" />
-              <Input fieldName="lastName" label="Last name" />
-              <Select
-                fieldName="iceCream"
-                label="Ice Cream"
-                options={[
-                  { label: 'Chocolate', value: 'chocolate' },
-                  { label: 'Vanilla', value: 'vanilla' },
-                  { label: 'Strawberry', value: 'strawberry' },
-                ]}
-              />
-              <Address fieldName="address" />
-              <Checkbox
-                fieldName="terms"
-                label="I agree to the terms and conditions"
-              />
-              <Email fieldName="email" label="Email" />
-
-              <button
-                aria-label="Increment value"
-                onClick={() => dispatch(increment())}
-              >
-                Increment
-              </button>
-              <span>{count}</span>
-              <button
-                aria-label="Decrement value"
-                onClick={() => dispatch(decrement())}
-              >
-                Decrement
-              </button>
-            </Form>
+            <Outlet />
           </Container>
         }
-      />
+      >
+        <Route
+          index
+          element={
+            <div>
+              <Typography>Let's get started</Typography>
+              <Link to="step-1">Step 1</Link>
+            </div>
+          }
+        />
+        <Route path="step-1" element={<Step />} />
+      </Route>
+      <Route path="*" element={<div>nope</div>} />
     </Routes>
   );
 }
--- apps/crud/src/app/components/Step/index.tsx
+++ apps/crud/src/app/components/Step/index.tsx
@@ -1,31 +1,53 @@
-import { createSlice } from '@reduxjs/toolkit'
-import type { PayloadAction } from '@reduxjs/toolkit'
+import { Address, Checkbox, Email, Form, Input, Select } from '@mui-plus/form';
+import { Container } from '@mui/material';
+import { Route } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { RootState } from '../../store';
+import { decrement, increment } from './stepSlice';
 
-export interface StepState {
-  value: number
+export function Step() {
+  const count = useSelector((state: RootState) => state.step.value);
+  const dispatch = useDispatch();
+
+  return (
+
+    <Container maxWidth="sm">
+      <Form onSubmit={() => { }}>
+        <Input fieldName="firstName" label="First name" />
+        <Input fieldName="lastName" label="Last name" />
+        <Select
+          fieldName="iceCream"
+          label="Ice Cream"
+          options={[
+            { label: 'Chocolate', value: 'chocolate' },
+            { label: 'Vanilla', value: 'vanilla' },
+            { label: 'Strawberry', value: 'strawberry' },
+          ]}
+        />
+        <Address fieldName="address" />
+        <Checkbox
+          fieldName="terms"
+          label="I agree to the terms and conditions"
+        />
+        <Email fieldName="email" label="Email" />
+
+        <button
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
+        >
+          Increment
+        </button>
+        <span>{count}</span>
+        <button
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </button>
+      </Form>
+    </Container>
+
+  );
 }
 
-const initialState: StepState = {
-  value: 0,
-}
-
-export const stepSlice = createSlice({
-  name: 'step',
-  initialState,
-  reducers: {
-    increment: (state) => {
-      state.value += 1
-    },
-    decrement: (state) => {
-      state.value -= 1
-    },
-    incrementByAmount: (state, action: PayloadAction<number>) => {
-      state.value += action.payload
-    },
-  },
-})
-
-// Action creators are generated for each case reducer function
-export const { increment, decrement, incrementByAmount } = stepSlice.actions
-
-export default stepSlice.reducer
+export default Step;
